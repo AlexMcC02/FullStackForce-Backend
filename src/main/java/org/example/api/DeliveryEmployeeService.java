868ba1a01c96bf128381439ba12acd59595ab58c
Conn Meehan <conn.meehan@kainos.com>
2023-07-06 11:31:45 +0100

    Updated Delivery Service and Controller to get Employee by ID

--- src/main/java/org/example/api/DeliveryEmployeeService.java
+++ src/main/java/org/example/api/DeliveryEmployeeService.java
@@ -1,2 +1,37 @@
-package org.example.api;public class DeliveryEmployeeService {
+package org.example.api;
+
+import org.example.client.DeliveryEmployeeDoesNotExistException;
+import org.example.client.FailedToGetDeliveryEmployeeException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DeliveryEmployeeService {
+    private DeliveryEmployeeDao deliveryEmployeeDao = new DeliveryEmployeeDao();
+
+    public List<DeliveryEmployee> getAllDeliveryEmployees() throws FailedToGetDeliveryEmployeeException {
+        try {
+            List<DeliveryEmployee> deliveryEmployeeList = deliveryEmployeeDao.getAllDeliveryEmployees();
+
+            return deliveryEmployeeList;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new FailedToGetDeliveryEmployeeException();
+        }
+    }
+
+    public DeliveryEmployee getDeliveryEmployeeById(int id) throws FailedToGetDeliveryEmployeeException, DeliveryEmployeeDoesNotExistException {
+        try {
+            DeliveryEmployee delivery_employee = deliveryEmployeeDao.getDeliveryEmployeeById(id);
+
+            if (delivery_employee == null) {
+                throw new DeliveryEmployeeDoesNotExistException();
+            }
+            return delivery_employee;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetDeliveryEmployeeException();
+        }
+    }
 }
--- src/main/java/org/example/client/FailedToGetDeliveryEmployeeException.java
+++ src/main/java/org/example/client/FailedToGetDeliveryEmployeeException.java
@@ -1,2 +1,8 @@
-package org.example.client;public class FailedToGetDeliveryEmployeeException {
+package org.example.client;
+
+public class FailedToGetDeliveryEmployeeException extends Exception {
+    @Override
+    public String getMessage(){
+        return "Failed to get delivery employee from the database";
+    }
 }
